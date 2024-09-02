fb8ed187ed006107478961c0f8de7cf624d637c0
therahulkumarofficial <rahulkumarofficial01@gmail.com>
2024-09-02 13:41:40 +0000

    all changes with password is hide now

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config(); 
 
+const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //req.body
 
@@ -41,7 +43,7 @@ app.use('/person', personRoutes);
 const menuItemRoutes = require('./routes/menuItemRoutes')
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('listening on port 3000');
 }) // 3000 is port
 
